a9f5ea52c440bf7ee46c7a509993a2af469c2ce7
bdaehz <cristiancabral826@gmail.com>
2025-04-11 16:44:36 -0300

    hola

--- red_social/landing/views.py
+++ red_social/landing/views.py
@@ -1,17 +1,9 @@
-
-from urllib import request
 from django.shortcuts import render, redirect
-
-#libreria para el auth
 from django.contrib.auth import authenticate, login, logout
-
-#formulario de registro
 from landing.forms import RegisterForm
-
-#mensaje de que se registro
 from django.contrib import messages
 
-# Create your views here.
+# Elimina la clase TwoFactorSetupWizard porque ya estamos usando SetupView de two-factor-auth
 
 def index(request):
     return render(request, 'index.html')
@@ -26,19 +18,8 @@ def register(request):
     return render(request, 'register.html', {'register_form': register_form})
 
 def Loadlogin(request):
-    # if request.method == 'POST':
-    #     nombre = request.POST.get('username')
-    #     contra = request.POST.get('password')
-    #     user = authenticate(request, username = nombre, password = contra)
-    #     if user is not None:
-    #         login(request, user)
-    #         return redirect('index')
-    #     else:
-    #         messages.warning(request, 'algo no piolo como tenia que piolar')
-    #         return redirect('login')
     return redirect('two_factor:login')
 
 def Logout(request):
     logout(request)
     return redirect('index')
-
--- red_social/red_social/urls.py
+++ red_social/red_social/urls.py
@@ -1,22 +1,6 @@
-"""
-URL configuration for red_social project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from two_factor.views import LoginView, ProfileView
+from two_factor.views import SetupView  # Vista predefinida de 2FA
 from landing import views
 
 urlpatterns = [
@@ -25,5 +9,6 @@ urlpatterns = [
     path('login/', views.Loadlogin, name='login'),
     path('register/', views.register, name='register'),
     path('LogOut/', views.Logout, name='LogOut'),
-    path('2faapp/', include(('two_factor.urls', 'two_factor')))
+    path('2faapp/', include(('two_factor.urls', 'two_factor'))),
+    path('2fa/setup/', SetupView.as_view(), name='setup_2fa'),  # Esta vista maneja el flujo de configuración de 2FA
 ]
