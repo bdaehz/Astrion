a9550c9f0441e49108fb57df673ed03cf9f4d327
bdaehz <cristiancabral826@gmail.com>
2025-04-25 16:19:30 -0300

    arreglo simple de css verificacion 2 pasos

--- red_social/landing/views.py
+++ red_social/landing/views.py
@@ -13,8 +13,8 @@ def register(request):
     if request.method == 'POST':
         if register_form.is_valid():
             register_form.save()
-            messages.success(request, 'Te registraste papu :vv')
-            return redirect('index')
+            messages.success(request, 'Te registraste con exito')
+            return redirect('login')
     return render(request, 'register.html', {'register_form': register_form})
 
 def Loadlogin(request):
